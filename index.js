9054c3b0cb61cee11d11467111164d62d107ff4a
Karim Deif <karimdeif@gmail.com>
2020-07-30 11:45:33 +0200

    add console message

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const { CloudPakForDataAuthenticator } = require('ibm-watson/auth');
 
 app.post('/query', function (req, res) {
 
+   console.log('test 21');
+   
    let req_search_entity = req.body.search_entity;
    let req_search_query = req.body.search_query;
    
@@ -22,10 +24,15 @@ app.post('/query', function (req, res) {
    //let collection_safelink  = 'f50dcb3d-e350-7fc6-0000-01737db75b7c' ;  
    //let collection_infomind  = 'f50dcb3d-e350-7fc6-0000-01737dd20e25';
 
-   let collection_ddos      = 'e9ef8ca7-154c-f424-0000-01739ee0e6dc';
+   //let collection_ddos      = 'e9ef8ca7-154c-f424-0000-01739ee0e6dc';
+   //let collection_safelink  = 'e9ef8ca7-154c-f424-0000-01739ee3414a' ;  
+   //let collection_infomind  = 'e9ef8ca7-154c-f424-0000-01739ee1a54c';   
+
+   let collection_ddos      = 'f50dcb3d-e350-7fc6-0000-01737d9bb618';
    let collection_safelink  = 'e9ef8ca7-154c-f424-0000-01739ee3414a' ;  
-   let collection_infomind  = 'e9ef8ca7-154c-f424-0000-01739ee1a54c';   
-   
+   let collection_infomind  = 'f50dcb3d-e350-7fc6-0000-01737dd20e25';   
+
+
    let selected_collection = '';
 
    if ((/ddos/i).test(req_search_entity)) {
